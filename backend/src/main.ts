8379f009a3026a93f71e18046d0f1ed479199c8a
gitkolihis <adelaniolanrewajuisrael@gmail.com>
2025-05-27 13:30:27 +0100

    add more custom message description based on endpoint's method

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
+import { HttpExceptionFilter } from './filters/httpsException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +20,12 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  // Setup Global Response Interceptor
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
+  // Setup Global Exception Filter
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 }
